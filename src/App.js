0d210ee6db971bc455040cd22aaaa11b8f58af45
Arijit Ghosh <arijitghosh828@gmail.com>
2023-12-28 18:19:04 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 // import Leftbar from "./components/leftbar/Leftbar";
 // import Navbar from "./components/navbar/Navbar";
 // import Rightbar from "./components/rightbar/Rightbar";
-import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom";
+// import Home from "./pages/home/Home";
+// import Profile from "./pages/profile/Profile";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
+// import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom";
 // import "./style.scss";
 // import { DarkModeContext } from "./context/darkModeContext";
 // import { AuthContext } from "./context/auth/authContext";
@@ -45,29 +45,9 @@ function App() {
 
   //   return children;
   // };
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />
-    },
-    {
-      path: "/profile/:id",
-      element: <Profile />
-    },
-    {
-      path: "/login",
-      element: <Login />
-    },
-    {
-      path: "/register",
-      element: <Register />
-    },
-  ])
-
   return (
     <div>
-      <RouterProvider router={router} />
+      <h1>Hello World!!!!!!!</h1>
     </div>
   );
 };
