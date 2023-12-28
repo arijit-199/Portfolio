3c588acba0f27852ff8dac94ef72957f2e09aaf4
Arijit Ghosh <arijitghosh828@gmail.com>
2023-12-28 21:56:51 +0530

    u

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+// import Login from "./pages/login/Login";
+
 
 
 function App() {
